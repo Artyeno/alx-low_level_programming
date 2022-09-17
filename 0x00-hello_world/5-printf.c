eb510d5861ec81754cdf5ba5a8bc4c3d3673a3d6
Artyeno <omolocolleta@gmail.com>
2022-09-17 15:36:45 -0700

    task5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - print the string in the main function
- * 
- * Description: using the main function
- * this program prints "with proper grammar, the outcome is a piece of art"
- * Return: 0
- * /
+*main - Prints a text
+*
+*Return: 0
+*/
 int main(void)
- {
-	printf("with proper grammar, the outcome is a piece of art,\n");
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
-
